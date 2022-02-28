bfc021a366a7105c5eb944388b0e768b2396c73c
gary <gary.white@lodgit.com>
2022-02-28 12:59:28 +0100

    code

---
+++ app.js
@@ -0,0 +1,12 @@
+function isPalindrome(x) {
+    // your code here
+}
+
+console.log(isPalindrome("a")); // true
+console.log(isPalindrome("aba")); // true
+console.log(isPalindrome("Abba")); // true
+console.log(isPalindrome("hello")); // false
+console.log(isPalindrome("Bob")); // true
+console.log(isPalindrome("Madam")); // true
+console.log(isPalindrome("AbBa")); // true
+console.log(isPalindrome("")); // true
